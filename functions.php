357b5cd65f114817a070ccd72cb3b05fd0df0550
Olami Lekan <47719236+olaceo@users.noreply.github.com>
2019-02-17 17:36:30 +0100

    Update functions.php
    Add ads after second paragraph of single post content for bloggers

--- functions.php
+++ functions.php
@@ -117,6 +117,38 @@ function cfct_widgets_init() {
 }
 add_action( 'widgets_init', 'cfct_widgets_init' );
 
+//Add ads after second paragraph of single post content.
+
+add_filter( 'the_content', 'prefix_insert_post_ads' );
+
+function prefix_insert_post_ads( $content ) {
+	
+	$ad_code = 'AD CODE HERE';
+
+	if ( is_single() && ! is_admin() ) {
+		return prefix_insert_after_paragraph( $ad_code, 2, $content );
+	}
+	
+	return $content;
+}
+ 
+function prefix_insert_after_paragraph( $insertion, $paragraph_id, $content ) {
+	$closing_p = '</p>';
+	$paragraphs = explode( $closing_p, $content );
+	foreach ($paragraphs as $index => $paragraph) {
+
+		if ( trim( $paragraph ) ) {
+			$paragraphs[$index] .= $closing_p;
+		}
+
+		if ( $paragraph_id == $index + 1 ) {
+			$paragraphs[$index] .= $insertion;
+		}
+	}
+	
+	return implode( '', $paragraphs );
+}
+
 /**
  * Enqueue's scripts and styles
  */
